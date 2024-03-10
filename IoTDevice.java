e6b4aec9263f53fa4cb922ac5ad65957791eb423
rockTog <blab55153@gmail.com>
2024-03-10 19:02:15 +0000

    Client Menu

--- IoTDevice.java
+++ IoTDevice.java
@@ -93,7 +93,180 @@ public class IoTDevice {
             System.out.println(msgDevId);
 
 
+            // File file = new File("IoTDevice.jar");
+            // String fileName = file.getName();
+            // long fileSize = file.length();
 
+            // // ObjectOutputStream out = new
+            // // ObjectOutputStream(clientSocket.getOutputStream());
+            // out.writeObject(fileName);out.writeObject(fileSize);
+
+            // Scanner sc = new Scanner(System.in);
+            while(true) {
+
+            System.out.println("Enter command:");
+            String command = scanner.nextLine();
+
+            if (command.startsWith("CREATE")) {
+                String[] parts = command.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    String domainName = parts[1];
+                    out.writeObject("CREATE");
+                    out.writeObject(domainName);
+                    out.flush();
+                }
+                String srvResponse = in.readObject();
+                System.out.println(srvResponse);
+                
+            } else if (command.startsWith("ADD")) {
+                String[] parts = command.split(" ");
+
+                if (parts.length != 3) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    String user = parts[1];
+                    String domainName = parts[2];
+                    out.writeObject("ADD");
+                    out.writeObject(user);
+                    out.writeObject(domainName);
+                    out.flush();
+                }
+                String srvResponse = in.readObject();
+                System.out.println(srvResponse);
+
+            } else if (command.startsWith("RD")) {
+                String[] parts = command.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    String domainName = parts[1];
+                    out.writeObject("RD");
+                    out.writeObject(domainName);
+                    out.flush();
+                }
+                String srvResponse = in.readObject();
+                System.out.println(srvResponse);
+
+            } else if (command.startsWith("ET")) {
+                String[] parts = command.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    float temperature = Float.parseFloat(parts[1]);
+                    out.writeObject("ET");
+                    out.writeFloat(temperature);
+                    out.flush();
+                }
+                String srvResponse = in.readObject();
+                System.out.println(srvResponse);
+
+            } else if (command.startsWith("EI")) {
+                String[] parts = command.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    File file = new File(parts[1]);
+                    byte[] buffer = new byte[(int) file.length()];
+                    in = new FileInputStream(file);
+                    BufferedInputStream bis = new BufferedInputStream(in);
+                    int bytesRead = in.read(buffer, 0, buffer.length);
+                    
+                    out.writeObject("EI");
+                    out.writeInt(bytesRead);
+                    out.write(buffer, 0, bytesRead);
+                    out.flush();
+                }
+                String srvResponse = in.readObject();
+                System.out.println(srvResponse);
+
+            } else if (command.startsWith("RT")) {  // print("OK" + " " + fileSize + " " + conteudo)
+                String[] parts = command.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    out.writeObject("RT");
+                    out.writeObject(parts[1]);
+                    out.flush();
+                }
+                String srvResponse = (String) in.readObject();
+                System.out.println(srvResponse.split(", ")[0]); // checkar regex de separação
+
+                if (srvResponse.startsWith("OK")) {;
+                    long fileSize = Long.parseLong(in.readObject());
+
+                    byte[] buffer = new byte[(int) fileSize];
+                    // FileOutputStream fos = new FileOutputStream("received_file.txt");
+                    // BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+                    int bytesRead = 0;
+                    int count;
+                    while (bytesRead < fileSize) {
+                        count = in.read(buffer);
+                        // bos.write(buffer, 0, count);
+                        bytesRead += count;
+                    }
+                    // bos.flush();
+                    // bos.close();
+                    String fileContent = new String(buffer);
+                    System.out.println(fileContent);
+                }
+
+            } else if (command.startsWith("RI")) { // print("OK" + " " + fileSize + " " + conteudo)
+                
+                String[] parts = command.split(" ");
+
+                if (parts.length != 2) {
+                    System.out.println("Invalid command");
+                    continue;
+                } else {
+                    out.writeObject("RI");
+                    out.writeObject(parts[1]);
+                    out.flush();
+                }
+                String srvResponse = (String) in.readObject();
+                System.out.println(srvResponse.split(" ")[0]);  // checkar regex de separação
+
+                if (srvResponse.startsWith("OK")) {
+                    long fileSize = Long.parseLong(in.readObject());
+                    
+                    byte[] buffer = new byte[(int) fileSize];
+                    // FileOutputStream fos = new FileOutputStream("received_file.jpg");
+                    // BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+                    int bytesRead = 0;
+                    int count;
+                    while (bytesRead < fileSize) {
+                        count = in.read(buffer);
+                        if (count == -1) break;
+                        // bos.write(buffer, 0, count);
+                        bytesRead += count;
+                    }
+                    String fileContent = new String(buffer);
+                    System.out.println(fileContent);
+                    // bos.flush();
+                    // bos.close();
+                }
+
+            } else {
+                System.out.println("Invalid command");
+                continue;
+            }
+            //Resposta do servidor
+            
+          }
 
 
 
